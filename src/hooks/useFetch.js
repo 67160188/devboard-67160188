24e89f7aeb3ea37969b7ee77b99b6426cc4337d4
Narupanat <67160188@go.buu.ac.th>
2026-03-17 20:05:59 +0700

    useFetch custom hook

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,30 @@
+// Challenge ⭐⭐⭐: Custom Hook useFetch
+import { useState, useEffect } from "react";
+
+function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setLoading(true);
+        setError(null);
+        const res = await fetch(url);
+        if (!res.ok) throw new Error("ดึงข้อมูลไม่สำเร็จ");
+        const json = await res.json();
+        setData(json);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+}
+
+export default useFetch;
